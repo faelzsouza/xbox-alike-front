499714b5672e5c50480cbeb3e29d7d1eb6bf0c00
Rafael Souza <faelzsouza@gmail.com>
2021-10-07 22:48:02 -0300

    chore[header]: add logo

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,10 +1,12 @@
 import './Header.scss'
 import React from 'react'
+import NavMenu from '../../structure/NavMenu/NavMenu'
 
 const Header = () => {
     return (
         <header>
-            Header
+            <img src="https://bloximages.chicago2.vip.townnews.com/pinalcentral.com/content/tncms/assets/v3/editorial/e/24/e24916cc-b8de-5c48-9fe7-94b44dc646c2/59c9913f22bda.image.gif" />
+            <NavMenu />
         </header>
     )
 }
