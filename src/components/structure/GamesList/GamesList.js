d29d83a59ced4a9fea8cf4ea8e73f4d03b0e32a7
Rafael Souza <faelzsouza@gmail.com>
2021-10-11 19:35:28 -0300

    chore[navmenu]: change load to skeleton

--- src/components/structure/GamesList/GamesList.js
+++ src/components/structure/GamesList/GamesList.js
@@ -3,18 +3,42 @@ import React, { useEffect, useState } from "react";
 import { Api } from "../../../api/api";
 import VideoEmbed from "../VideoEmbed/VideoEmbed";
 import GameCard from "../GameCard/GameCard";
+import ContentLoader from "react-content-loader"
 
 const GamesList = () => {
-    const [ games, setGames ] = useState([]);
+    const [ games, setGames ] = useState(undefined);
 
     const fetchGetAll = async () => {
-        const response = await Api.getAll();
+        const response = await Api.getAll('games');
         const res = await response.json();
         setGames(res);
     };
 
     useEffect(() => fetchGetAll(), []);
-    console.log(games)
+
+    if(!games){
+        const MyLoader = (props) => (
+            <ContentLoader 
+              speed={2}
+              width={280}
+              height={320}
+              viewBox="0 0 320 320"
+              backgroundColor="#dedede"
+              foregroundColor="#4a4a4a"
+              {...props}
+            >
+              <rect x="9" y="2" rx="2" ry="2" width="141" height="214" /> 
+              <rect x="277" y="451" rx="0" ry="0" width="11" height="0" /> 
+              <rect x="37" y="229" rx="0" ry="0" width="85" height="18" /> 
+              <rect x="37" y="255" rx="0" ry="0" width="85" height="18" /> 
+              <rect x="163" y="3" rx="2" ry="2" width="141" height="214" /> 
+              <rect x="191" y="230" rx="0" ry="0" width="85" height="18" /> 
+              <rect x="191" y="256" rx="0" ry="0" width="85" height="18" />
+            </ContentLoader>
+          )
+        return <MyLoader />
+    }
+
     return (
         <div className="games-group">
             <GameCard games={games} />
