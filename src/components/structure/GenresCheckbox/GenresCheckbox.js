87e9fdd35efd408857d5fcb979129d89944eaa6d
Rafael Souza <faelzsouza@gmail.com>
2021-10-11 19:33:06 -0300

    feat[genrescheckbox]: add and increment a checkbox list of genres on new game page

---
+++ src/components/structure/GenresCheckbox/GenresCheckbox.js
@@ -0,0 +1,64 @@
+import {
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Api } from "../../../api/api";
+import NewGenre from "../NewGenre/NewGenre";
+
+const GenresCheckbox = ({ onGenresChecked }) => {
+    const [genres, setGenres] = useState(undefined);
+    const [genresChecked, setGenresChecked] = useState([]);
+    
+    const [newGenres, setNewGenres ] = useState(false)
+
+    const getAllGenres = async () => {
+        const response = await Api.getAll("genres");
+        const result = await response.json();
+        setGenres(result);
+    };
+    
+    useEffect(() => {
+        getAllGenres()
+        setNewGenres(false)
+    }, [newGenres]);
+    
+    if (!genres) {
+        return "Loading...";
+    }
+
+    const handleChange = (event) => {
+        const checked = event.target.checked;
+        const value = event.target.value;
+        const list = [...genresChecked];
+        if (checked) {
+            list.push(value);
+        } else {
+            list.splice(list.indexOf(value), 1);
+        }
+        setGenresChecked(list);
+        onGenresChecked(list)
+    };
+    return (
+        <FormControl sx={{ marginTop: "1rem" }} component="fieldset">
+            <FormLabel component="legend">Genres</FormLabel>
+            <FormGroup aria-label="position" row>
+                {genres.map((genre, idx) => (
+                    <FormControlLabel
+                        value={genre.name}
+                        control={<Checkbox onChange={handleChange} />}
+                        label={genre.name}
+                        labelPlacement="end"
+                        key={`list-genre${idx}`}
+                    />
+                ))}
+            </FormGroup>
+            <NewGenre setNewGenres={setNewGenres} />
+        </FormControl>
+    );
+};
+
+export default GenresCheckbox;
