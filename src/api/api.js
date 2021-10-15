4a93499313cc6ab5575929eca3e7bed0de53f380
Rafael Souza <faelzsouza@gmail.com>
2021-10-15 16:05:06 -0300

    feat[many]: create all CRUD fetchs

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,43 @@
 export const Api = {
-    baseUrl: ["https://pokeapi.co/api/v2/", "http://localhost:3000"],
-    getAll: () => fetch(Api.baseUrl[1] + "/games")
-}
+    baseUrl: [
+        "https://pokeapi.co/api/v2/",
+        "http://localhost:3000",
+        "https://back-xboxalike.herokuapp.com",
+    ],
+    getAll: (tableName, auth) =>
+        fetch(Api.baseUrl[1] + `/${tableName}`, getRequest(auth)),
+    insert: (tableName, body, auth) =>
+        fetch(Api.baseUrl[1] + `/${tableName}`, postRequest(body, auth)),
+    login: (body) => fetch(Api.baseUrl[1] + "/login", postRequest(body)),
+    authHeader: () => ({
+        Authorization: "Bearer " + localStorage.getItem("JWT"),
+    }),
+};
 
-/* "pokemon?offset=0&limit=50" */
+const getRequest = (auth) => ({
+    method: "GET",
+    headers: auth ? new Headers(Api.authHeader()) : undefined,
+});
+
+const postRequest = (body, auth) => ({
+    method: "POST",
+    headers: new Headers({
+        "Content-type": "application/json",
+        ...(auth ? Api.authHeader() : {}),
+    }),
+    body: JSON.stringify(body),
+});
+
+const patchRequest = (body, auth) => ({
+    method: "PATCH",
+    headers: new Headers({
+        "Content-type": "application/json",
+        ...(auth ? Api.authHeader() : {}),
+    }),
+    body: JSON.stringify(body),
+});
+
+const deleteRequest = (auth) => ({
+    method: "DELETE",
+    headers: auth ? new Headers(Api.authHeader()) : undefined,
+});
