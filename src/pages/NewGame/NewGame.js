9fb16d9b9272500fea6b36a7b52cdbb5f133e248
Rafael Souza <faelzsouza@gmail.com>
2021-10-15 15:38:52 -0300

    refact[many]: add button to form, implement a handleSubmit, export BoxEdited and TextFieldEdited components

--- src/pages/NewGame/NewGame.js
+++ src/pages/NewGame/NewGame.js
@@ -2,29 +2,48 @@ import './NewGame.scss'
 
 import React from 'react'
 import { Box, styled } from '@mui/system'
-import { TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import GenresCheckbox from '../../components/structure/GenresCheckbox/GenresCheckbox'
-import NewGenre from '../../components/structure/NewGenre/NewGenre'
+import { Api } from '../../api/api'
 
-const NewGame = () => {
+const NewGame = (props) => {
     const handleGenresCheck = (checked) => {
         console.log(checked)
     }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+/*         console.log(1, event)
+        return */
+        const payload = {
+            title: event.target.title.value,
+            cover: event.target.cover.value,
+            description: event.target.description.value,
+            year: +event.target.year.value,
+            imdb: +event.target.imdb.value,
+            trailer: event.target.trailer.value,
+            gameplay: event.target.gameplay.value
+        }
+        console.log(payload)
+        console.log(props)
+        Api.insert('games', payload, true)
+        props.history.push('/game/new')
+    }
     return (
-        <BoxEdited component="form">
-            <TextFieldEdited label="Title" name='title' />
-            <TextFieldEdited label="Cover Link" name='cover' />
-            <TextFieldEdited label="Description" name='description' />
-            <TextFieldEdited label="Release Year" name='year' />
-            <TextFieldEdited label="IMDB Rate" name='imdb' />
+        <BoxEdited component="form" onSubmit={handleSubmit}>
+            <TextFieldEdited label="Title" name='title' required />
+            <TextFieldEdited label="Cover Link" name='cover' required />
+            <TextFieldEdited label="Description" name='description' required />
+            <TextFieldEdited label="Release Year" name='year' type='number' />
+            <TextFieldEdited label="IMDB Rate" name='imdb' type='number' />
             <TextFieldEdited label="Embed Trailer Link" name='trailer' />
             <TextFieldEdited label="Embed Gameplay Link" name='gameplay' />
             <GenresCheckbox onGenresChecked={handleGenresCheck} />
+            <Button type="submit">Submit</Button>
         </BoxEdited>
     )
 }
 
-const BoxEdited = styled(Box)(`
+export const BoxEdited = styled(Box)(`
     display: flex;
     flex-direction: column;
     width: 80vw;
@@ -32,7 +51,7 @@ const BoxEdited = styled(Box)(`
     max-width: 500px;
 `)
 
-const TextFieldEdited = styled(TextField)(`
+export const TextFieldEdited = styled(TextField)(`
     margin-top: 1rem;
 `)
 
