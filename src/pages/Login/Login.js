85b655bf599370ca204bf333ae5dedeafa7da26e
Rafael Souza <faelzsouza@gmail.com>
2021-10-15 15:39:34 -0300

    feat[login]: create and implement a login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,42 @@
+import { Box, Button, TextField } from '@mui/material'
+import React from 'react'
+import { Api } from '../../api/api';
+import { TextFieldEdited } from '../NewGame/NewGame';
+
+const Login = (props) => {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        const payload = {
+            email,
+            password
+        }
+
+        const response = await Api.login(payload)
+
+        const body = await response.json()
+
+        if (response.status === 200){
+
+            const accessToken = body.accessToken;
+            localStorage.setItem("JWT", accessToken)
+            console.log(accessToken)
+
+            props.history.push("/")
+        }
+    }
+
+    return (
+        <Box component="form"
+        onSubmit={handleSubmit}>
+            <TextFieldEdited label="E-mail" name="email" />
+            <TextFieldEdited label="Password" name='password' type="password" />
+            <Button type='submit'>Send</Button>
+        </Box>
+    )
+}
+
+export default Login
