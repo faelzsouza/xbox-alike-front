23e9abc23dfef887bca6ca095378a90f20bcc61c
Rafael Souza <faelzsouza@gmail.com>
2021-10-15 16:03:42 -0300

    refact[Route]: set `Home` and `NewGame` paths to GuardedRoute

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { Route, Switch } from "react-router";
 import Header from "./components/shared/Header/Header";
 import Footer from "./components/shared/Footer/Footer";
 import Home from "./pages/Home/Home";
+import NewGame from "./pages/NewGame/NewGame";
+import GuardedRoute from "./components/auth/GuardedRoute/GuardedRoute";
+import Login from "./pages/Login/Login";
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Switch>
-              <Route path='/' exact={true} component={Home} />
+                <Route path="/login" component={Login} />
+                <GuardedRoute path="/" exact={true} component={Home} />
+                <GuardedRoute path="/game/new" component={NewGame} />
             </Switch>
             <Footer />
         </div>
