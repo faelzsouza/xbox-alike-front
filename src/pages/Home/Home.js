e3fefe2ce4dc2ba883848cf219f4c863b9b57b9c
Rafael Souza <faelzsouza@gmail.com>
2021-10-15 15:40:18 -0300

    chore[Home]: remove `Home` text

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,6 @@ import GamesList from '../../components/structure/GamesList/GamesList'
 const Home = () => {
     return (
         <div>
-            Home
             <GamesList />
         </div>
     )
