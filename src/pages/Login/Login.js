52d35c03e32011eb37dfac19dbf8372e9ae4f160
Rafael Souza <faelzsouza@gmail.com>
2021-10-15 23:10:36 -0300

    refact[handleSubmit]: change `localStorage` to `JwtHandler`

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,10 @@
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
-import { Api } from '../../api/api';
-import { TextFieldEdited } from '../NewGame/NewGame';
+import { Box, Button, TextField } from "@mui/material";
+import React from "react";
+import { Api } from "../../api/api";
+import { JwtHandler } from "../../local-storage/jwt-handler";
+import { TextFieldEdited } from "../NewGame/NewGame";
 
 const Login = (props) => {
-
     const handleSubmit = async (event) => {
         event.preventDefault();
         const email = event.target.email.value;
@@ -12,31 +12,29 @@ const Login = (props) => {
 
         const payload = {
             email,
-            password
-        }
+            password,
+        };
 
-        const response = await Api.login(payload)
+        const response = await Api.login(payload);
 
-        const body = await response.json()
+        const body = await response.json();
 
-        if (response.status === 200){
-
+        if (response.status === 200) {
             const accessToken = body.accessToken;
-            localStorage.setItem("JWT", accessToken)
-            console.log(accessToken)
+            JwtHandler.setJwt(accessToken);
+            console.log(accessToken);
 
-            props.history.push("/")
+            props.history.push("/");
         }
-    }
+    };
 
     return (
-        <Box component="form"
-        onSubmit={handleSubmit}>
+        <Box component="form" onSubmit={handleSubmit}>
             <TextFieldEdited label="E-mail" name="email" />
-            <TextFieldEdited label="Password" name='password' type="password" />
-            <Button type='submit'>Send</Button>
+            <TextFieldEdited label="Password" name="password" type="password" />
+            <Button type="submit">Send</Button>
         </Box>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
