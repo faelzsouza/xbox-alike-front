b986e740dac6b18780002fb3df93f6e082596152
Rafael Souza <faelzsouza@gmail.com>
2021-10-21 03:30:54 -0300

    feat[ErrorSnackBar]: implement an error snackbar if auth error

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,13 @@
-import { Button } from "@mui/material";
-import React from "react";
+import { Button, Snackbar, Stack } from "@mui/material";
+import React, { forwardRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Api } from "../../api/api";
 import { JwtHandler } from "../../local-storage/jwt-handler";
 import { BoxEdited, TextFieldEdited } from "../NewGame/NewGame";
+import MuiAlert from '@mui/material/Alert';
 
 const Login = (props) => {
+    const [error, setError] = useState(false)
     const handleSubmit = async (event) => {
         event.preventDefault();
         const email = event.target.email.value;
@@ -23,23 +25,68 @@ const Login = (props) => {
         if (response.status === 200) {
             const accessToken = body.accessToken;
 
-            localStorage.setItem('userId', body.userId);
+            localStorage.setItem("userId", body.userId);
 
             // salva o JWT no localStorage
             JwtHandler.setJwt(accessToken);
 
             props.history.push(`/profiles`);
+        } else {
+            setError(true)
         }
     };
 
     return (
         <BoxEdited component="form" onSubmit={handleSubmit}>
-            <TextFieldEdited label="E-mail" name="email" />
-            <TextFieldEdited label="Password" name="password" type="password" />
-            <Button type="submit" variant="contained" sx={{width: "10rem", display: "block", margin: "1rem auto"}}>Sign in</Button>
-            <Button sx={{width: "10rem", display: "block", margin: "1rem auto"}}><Link to='/user/create'>Sign up</Link></Button>
+            <TextFieldEdited label="E-mail" name="email" required />
+            <TextFieldEdited
+                label="Password"
+                name="password"
+                type="password"
+                required
+            />
+            <Button
+                type="submit"
+                variant="contained"
+                sx={{ width: "10rem", display: "block", margin: "1rem auto" }}
+            >
+                Sign in
+            </Button>
+            <Button
+                sx={{ width: "10rem", display: "block", margin: "1rem auto" }}
+            >
+                <Link to="/user/create">Sign up</Link>
+            </Button>
+            {error && <ErrorSnackbar />}
         </BoxEdited>
     );
 };
+
+const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export function ErrorSnackbar() {
+    const [open, setOpen] = useState(true);
+  
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+      setOpen(false);
+
+      window.location.reload();
+    };
+  
+    return (
+      <Stack spacing={2} sx={{ width: '100%' }}>
+        <Snackbar open={open} autoHideDuration={1500} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            E-mail or password invalid! Try again!
+          </Alert>
+        </Snackbar>
+      </Stack>
+    );
+  }
 
 export default Login;
