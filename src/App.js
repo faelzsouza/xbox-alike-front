af1c8ce6501b2f05a87a4b95fadeec0ebc2d1564
Rafael Souza <faelzsouza@gmail.com>
2021-10-20 23:48:10 -0300

    feat: add `profiles list`, `favorite games`, `create profile`, `edit game`, `create user` routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
 import { Route, Switch } from "react-router";
-import Header from "./components/shared/Header/Header";
-import Footer from "./components/shared/Footer/Footer";
-import Home from "./pages/Home/Home";
-import NewGame from "./pages/NewGame/NewGame";
+import "./App.css";
 import GuardedRoute from "./components/auth/GuardedRoute/GuardedRoute";
+import Footer from "./components/shared/Footer/Footer";
+import Header from "./components/shared/Header/Header";
+import FavoriteGames from "./pages/Home/components/FavoriteGames/FavoriteGames";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Logout from "./pages/Logout/Logout";
+import NewGame from "./pages/NewGame/NewGame";
+import NewProfile from "./pages/NewProfile/NewProfile";
+import NewUser from "./pages/NewUser/NewUser";
 import Profiles from "./pages/Profiles/Profiles";
 
 function App() {
@@ -17,7 +20,14 @@ function App() {
                 <Route path="/login" component={Login} />
                 <GuardedRoute path="/" exact={true} component={Home} />
                 <GuardedRoute path="/game/new" component={NewGame} />
-                <GuardedRoute path="/profiles/:userId" component={Profiles} />
+                <GuardedRoute path="/profiles" component={Profiles} />
+                <GuardedRoute
+                    path="/games/favorites"
+                    component={FavoriteGames}
+                />
+                <GuardedRoute path="/profile/create" component={NewProfile}/>
+                <GuardedRoute path="/game/edit" component={NewGame}/>
+                <Route path="/user/create" component={NewUser} />
                 <Route path="/logout" component={Logout} />
             </Switch>
             <Footer />
