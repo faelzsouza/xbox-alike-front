1f09ee45f7cc95f91a375a25cfeb6b21078b9fac
Rafael Souza <faelzsouza@gmail.com>
2021-10-21 19:22:57 -0300

    feat[FavoriteGames]: add custom message if return an empty favorite games list.

--- src/pages/Home/components/FavoriteGames/FavoriteGames.js
+++ src/pages/Home/components/FavoriteGames/FavoriteGames.js
@@ -1,7 +1,9 @@
+import { Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Api } from "../../../../api/api";
 import GameCard from "../GameCard/GameCard";
+import "./FavoriteGames.scss";
 
 const FavoriteGames = () => {
     const [profile, setProfile] = useState(undefined);
@@ -14,7 +16,27 @@ const FavoriteGames = () => {
     }, []);
 
     if (profile) {
-        return <GameCard games={profile.favGames} _selected={true} />;
+        if (profile.favGames.length == 0) {
+            return (
+                <div>
+                    There's no favorite games yet! Go to home page and click on
+                    heart button of your favorite games.
+                </div>
+            );
+        } else if (profile.favGames.length > 0) {
+            return (
+                <div className="favorite-games">
+                    <Typography
+                        variant="h5"
+                        component="h2"
+                        sx={{ marginTop: "1rem" }}
+                    >
+                        Favorite Games
+                    </Typography>
+                    <GameCard games={profile.favGames} _selected={true} />
+                </div>
+            );
+        }
     }
 
     return <div></div>;
